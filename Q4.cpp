cfa533f8e8c97199b90d38c6ad1ed37dc9ccdb64
smability <39351044+smability@users.noreply.github.com>
2018-09-24 11:06:51 -0500

    Create Q4.cpp

---
+++ Q4.cpp
@@ -0,0 +1,11 @@
+/*Q4: Implement a singleton C++ class.*/
+
+/*
+1. Define a private static attribute in the 'single instace' class
+2. Define a public static accessor function in the class
+3. Do "lazy initialization" in the acessor function
+4. Define all constructors to be protected or private
+5. Clients may only use the accessor function to manipulate the Singleton
+6. Inheritance can be supported, but static function may not be overriden.
+   The base class  must be delcared a friend of the derived class (in order to access the protected constructor)
+*/
