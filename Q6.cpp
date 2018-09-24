c1fb8e4aa2277ec94689cad5fa4e2e60706e4d0d
smability <39351044+smability@users.noreply.github.com>
2018-09-24 10:49:57 -0500

    Create Q6.cpp

---
+++ Q6.cpp
@@ -0,0 +1,53 @@
+/*
+Q6. An array of size n is filled with intergers 1 to n+1. Write fucntion to find the missing number between 1 and n+1.
+example : inputarry[6] = {1,2,3,5,6,7}
+Here the last number should have been 6. But it is 7 because we skipped 4.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int n[6]={1,2,3,4,5,6};
+
+int length = sizeof(n)/sizeof(int); //length of an integer array
+
+/*function declaration*/
+int missingnumber(int n[]);
+
+/*function definition*/
+int missingnumber(int n[]){
+        
+        int i = 0;
+        int result;
+        
+        //5 is the max index of n[] lenght=6
+        while(i<length){ //array range
+            
+            if(n[i+1]-n[i] == 1){
+                i++; //next comparision 
+            }
+            else if(n[i+1]-n[i] == 2){  
+                result = n[i+1]-1; //result is the missing number
+                i++;
+            }
+            else{
+                //result never gets assigned
+                i++;
+            }
+            
+        }
+        return result; //output last missing number
+    }
+
+
+int main() {
+    
+    int solution = missingnumber(n);
+    
+    cout<<"Missing number in n[] is: " << solution;
+    
+    return 0; //do we need to return 0?
+}
+	
+
